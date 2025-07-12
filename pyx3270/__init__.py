fae58a5c10ce599a60d1382cb2b72a7d78fde10f
MatheusLPolidoro <mattpolidoro4@gmail.com>
2025-07-12 12:55:30 -0300

    refact: apontamento para cli

--- pyx3270/__init__.py
+++ pyx3270/__init__.py
@@ -11,13 +11,8 @@ Emulador de terminal 3270
 
 """
 
-import pathlib
-import sys
-
-sys.path.append(str(pathlib.Path(__file__).parent))
-
-from .emulator import X3270
-from .__main__ import replay, record
+from pyx3270.cli import record, replay
+from pyx3270.emulator import X3270
 
 __author__ = 'MatheusLPolidoro'
 __version__ = '0.1.0'
